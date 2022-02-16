da705c4fc31d9ec1863c400d1958a1fe9afdf6a8
Stephan Cilliers <hnzkqujglqhhetkdvrpizyald@forwardmail.rocks>
2022-02-16 20:54:13 +0200

    Fix search, update neon text size

--- client/src/components/NeonText/NeonText.tsx
+++ client/src/components/NeonText/NeonText.tsx
@@ -1,17 +1,13 @@
 import style from "./NeonText.module.css"
 import neonHead from'./NeonHead.png';
 
-interface NeonTextProps {
-  text: string
-}
-
 export const NeonText = () => {
   return <div style={{textAlign: "center"}}>
     <div className={style.neonHeadCont} style={{marginTop:"60px"}}>
       <img alt="Synthetic Punk" src={neonHead} className={style.neonHeadImg}></img>
     </div>
     <h1 className={style.neonGlow} style={{marginTop:"30px"}}>
-      Crypto Punks
+      Synthetic Punks
     </h1>
     <h2 className={style.neonText1} style={{marginTop:"-30px"}}>
     CryptoPunks for everyone
